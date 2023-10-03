fd82dffd6f217ebf6eb1ddfe252d7f3f5813ae24
David <Davidmenher17@gmail.com>
2023-10-03 10:54:11 -0500

    Se agrego el Crud Departamento

--- GestionNomina/urls.py
+++ GestionNomina/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import DepartamentoListView, DepartamentoCreateView,DepartamentoUpdateView,DepartamentoDeleteView
 
 urlpatterns = [
     path('', views.BASE,name='BASE' ),
-
+    path('departamentos/', DepartamentoListView.as_view(),name='departamento-list'),
+    path('departamentos/crear/', DepartamentoCreateView.as_view(), name='departamento_create.html'),
+    path('departamentos/editar/<int:pk>/', DepartamentoUpdateView.as_view(), name='departamento_update'),
+    path('departamentos/eliminar/<int:pk>/', DepartamentoDeleteView.as_view(), name='departamento-delete'),
 ]
--- GestionNomina/views.py
+++ GestionNomina/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView,UpdateView,DeleteView
+from .models import Departamento
 # Create your views here.
-
 def BASE(request):
     return render(request, 'base.html')
 
-    
+# Lista de departamentos
+class DepartamentoListView(ListView):
+    model = Departamento
+    template_name = 'departamento_list.html'
+    context_object_name = 'departamentos'
+
+# Crear un departamento
+class DepartamentoCreateView(CreateView):
+    model = Departamento
+    template_name = 'departamento_create.html'
+    fields = ['nom_departamento', 'estado']
+    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de crear uno
+
+# Actualizar un departamento
+
+class DepartamentoUpdateView(UpdateView):
+    model = Departamento
+    template_name = 'departamento_update.html'
+    fields = ['nom_departamento', 'estado']
+    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de crear uno
+
+# Eliminar un departamento
+
+class DepartamentoDeleteView(DeleteView):
+    model = Departamento
+    template_name = 'departamento_delete.html'
+    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de eliminar uno
