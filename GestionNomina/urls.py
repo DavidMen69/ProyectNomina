ce355eef3a3d85f4cf43ebe584b639dac6f12b81
David <Davidmenher17@gmail.com>
2023-10-03 22:13:40 -0500

    Actulizacion Cargos V2.0

--- GestionNomina/urls.py
+++ GestionNomina/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from . import views
 from .views import DepartamentoListView, DepartamentoCreateView,DepartamentoUpdateView,DepartamentoDeleteView
-
+from .views import CargoListView, CargoCreateView,CargoUpdateView,CargoDeleteView
 urlpatterns = [
     path('', views.BASE,name='BASE' ),
+    # --------------------model Departamento ---------------------------------
     path('departamentos/', DepartamentoListView.as_view(),name='departamento-list'),
-    path('departamentos/crear/', DepartamentoCreateView.as_view(), name='departamento_create.html'),
+    path('departamentos/crear/', DepartamentoCreateView.as_view(), name='departamento_create'),
     path('departamentos/editar/<int:pk>/', DepartamentoUpdateView.as_view(), name='departamento_update'),
     path('departamentos/eliminar/<int:pk>/', DepartamentoDeleteView.as_view(), name='departamento-delete'),
+
+    # ------------------Inicio Del Model Cargo--------------------------------
+    path('cargos/', CargoListView.as_view(),name='cargo_list'),
+    path('cargos/crear/', CargoCreateView.as_view(), name='cargo_create'),
+    path('cargos/editar/<int:pk>/', CargoUpdateView.as_view(), name='cargo_update'),
+    path('cargos/eliminar/<int:pk>/', CargoDeleteView.as_view(), name='cargo_delete'),
+
 ]
--- GestionNomina/views.py
+++ GestionNomina/views.py
@@ -1,34 +1,77 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView,UpdateView,DeleteView
-from .models import Departamento
-# Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Departamento, Cargo
+
+# Define una vista llamada BASE que renderiza la plantilla 'base.html'.
 def BASE(request):
     return render(request, 'base.html')
 
-# Lista de departamentos
+# Define una vista de lista para los departamentos.
 class DepartamentoListView(ListView):
+    # Establece el modelo que se utilizará para esta vista.
     model = Departamento
+    # Establece la plantilla que se utilizará para renderizar la lista de departamentos.
     template_name = 'departamento_list.html'
+    # Establece el nombre de contexto que se utilizará en la plantilla para referirse a la lista de departamentos.
     context_object_name = 'departamentos'
 
-# Crear un departamento
+# Define una vista para crear un nuevo departamento.
 class DepartamentoCreateView(CreateView):
+    # Establece el modelo que se utilizará para crear el departamento.
     model = Departamento
+    # Establece la plantilla que se utilizará para el formulario de creación de departamento.
     template_name = 'departamento_create.html'
+    # Especifica los campos del modelo que se mostrarán en el formulario.
     fields = ['nom_departamento', 'estado']
-    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de crear uno
+    # Establece la URL a la que se redirigirá después de crear exitosamente un departamento.
+    success_url = '/departamentos/'
 
-# Actualizar un departamento
-
+# Define una vista para actualizar un departamento existente.
 class DepartamentoUpdateView(UpdateView):
+    # Establece el modelo que se utilizará para actualizar el departamento.
     model = Departamento
+    # Establece la plantilla que se utilizará para el formulario de actualización de departamento.
     template_name = 'departamento_update.html'
+    # Especifica los campos del modelo que se mostrarán en el formulario de actualización.
     fields = ['nom_departamento', 'estado']
-    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de crear uno
+    # Establece la URL a la que se redirigirá después de actualizar exitosamente un departamento.
+    success_url = '/departamentos/'
 
-# Eliminar un departamento
-
+# Define una vista para eliminar un departamento existente.
 class DepartamentoDeleteView(DeleteView):
+    # Establece el modelo que se utilizará para eliminar el departamento.
     model = Departamento
+    # Establece la plantilla que se utilizará para la confirmación de eliminación de departamento.
     template_name = 'departamento_delete.html'
-    success_url = '/departamentos/'  # Redirigir a la lista de departamentos después de eliminar uno
+    # Establece la URL a la que se redirigirá después de eliminar exitosamente un departamento.
+    success_url = '/departamentos/'
+
+# ------------------Inicio Del Model Cargo--------------------------------
+
+# Lista de Cargo
+class CargoListView(ListView):
+    model = Cargo
+    template_name = 'cargo_list.html'
+    context_object_name = 'cargos'
+
+# Crear un Cargo
+class CargoCreateView(CreateView):
+    model = Cargo
+    template_name = 'cargo_create.html'
+    fields = ['nombre_cargo', 'estado']
+    success_url = '/cargos/'  # Redirigir a la lista de cargo después de crear uno
+
+# Actualizar un Cargo
+
+class CargoUpdateView(UpdateView):
+    model = Cargo
+    template_name = 'cargo_update.html'
+    fields = ['nombre_cargo', 'estado']
+    success_url = '/cargos/'  # Redirigir a la lista de departamentos después de crear uno
+
+# Eliminar un Cargo
+
+class CargoDeleteView(DeleteView):
+    model = Cargo
+    template_name = 'cargo_delete.html'
+    success_url = '/cargos/'  # Redirigir a la lista de departamentos después de eliminar uno
